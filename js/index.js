56d84e533727df416314070bfc400b8e83cb100f
Mitchell Wright <mitchellbwright@gmail.com>
2020-06-03 18:26:45 -0600

    change bus image when clicked

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-// Your code goes here
+// Resize main bus image when clicked
+let heroImage = document.querySelectorAll('.intro > img');
+heroImage[0].addEventListener('click', event => {
+    if(event.target.style.width === '50%') {
+        event.target.style.width = '100%';
+    } else {
+        event.target.style.width = '50%';
+    }
+});
